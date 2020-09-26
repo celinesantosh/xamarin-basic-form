97e5928f1cce524402d7e87a19390eee3e0edcb1
celinesantosh@gmail.com <celinesantosh@gmail.com>
2020-09-26 16:23:56 +0530

    importing project

---
+++ basicform/basicform/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace basicform
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+             
+
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+    }
+}
